6df653b2548a23d68ee641c5e2289f19dd0176ca
songbinliu <apple.songbin@gmail.com>
2017-08-25 17:44:15 -0400

    nits

--- script/run.sh
+++ script/run.sh
@@ -5,8 +5,8 @@ mybin=./_output/kubelet
 ## get monitor information from Kubernetes cluster
 
 kubeconfig="./config/aws.kubeconfig.yaml"
-host="enmaster.octurbo.org"
-masterUrl="https://x.x.x.x:6443
+host="10.10.172.33"
+masterUrl="https://x.x.x.x:6443"
 
 options=" --v=3"
 options="$options --kubeletPort=10255"
