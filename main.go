afca5eeec74e3f553554d167cac0922de00b7397
songbinliu <songbin.liu@turbonomic.com>
2017-08-25 17:47:40 -0400

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func process_flags(c *kubeletConfig) {
 	flag.StringVar(&kubeConfig, "kubeConfig", "", "path to the kubeconfig(admin.kubeconfig) file.")
 	flag.IntVar(&(c.timeout), "timeout", c.timeout, "the timeout when connecting to kubelet to get metrics.")
 	flag.IntVar(&(c.port), "kubeletPort", c.port, "the Port of kubelet to get metrics.")
-	flag.BoolVar(&(c.enableHttps), "kubeletHttps", c.enableHttps, "Access the kubelet with https or http.")
+	flag.BoolVar(&(c.enableHttps), "kubeletHttps", c.enableHttps, "Whether to access the kubelet with https or not(http).")
 	flag.StringVar(&kubeletHost, "host", kubeletHost, "the host of the kubelet")
 
 	flag.Set("alsologtostderr", "true")
